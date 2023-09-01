343d5d97e9107e00f960bb6f909c90befc29b799
vishu24262 <34614141+vishu24262@users.noreply.github.com>
2023-09-01 19:54:54 +0530

    Update pgsql.sql

--- pgsql.sql
+++ pgsql.sql
@@ -1,5 +1,5 @@
 -- Insert data into the 'example_table'
-INSERT INTO balic_table (name, age) VALUES
+INSERT INTO balic_table (id, name, age) VALUES
     (1, 'Data 1', 30),
     (2, 'Data 2', 25),
     (3, 'Data 3', 35);
