2ed5e4474868d48902eb53e16b29e785a29c1e00
vishu24262 <34614141+vishu24262@users.noreply.github.com>
2023-09-01 19:51:27 +0530

    Update pgsql.sql

--- pgsql.sql
+++ pgsql.sql
@@ -1,10 +1,3 @@
--- Create a table named 'example_table'
-CREATE TABLE balic_table_1 (
-    id serial,
-    name varchar(255),
-    age int
-);
-
 -- Insert data into the 'example_table'
 INSERT INTO balic_table_1 (name, age) VALUES
     (1, 'Data 1', 30),
