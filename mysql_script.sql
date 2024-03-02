869daf1af20f006d4d68e0cc9004fde840fab93e
vishu24262 <34614141+vishu24262@users.noreply.github.com>
2024-03-02 12:00:43 +0530

    Delete mysql_script.sql

--- mysql_script.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS balic_table (           
-  message varchar(255) UNIQUE NOT NULL         
-  ) ENGINE=MyISAM DEFAULT CHARSET=utf8;         
-  INSERT IGNORE INTO balic_table(message) VALUES('Sample Data inserted');
-  INSERT IGNORE INTO balic_table(message) VALUES('For BALIC POC');         
-  INSERT IGNORE INTO balic_table(message) VALUES('sample Data ends')
